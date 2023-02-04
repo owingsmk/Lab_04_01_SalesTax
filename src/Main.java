ced721af46c3d0d0aa9dddf6c26828ca62e36e06
Mark Owings <owingsmk@mail.uc.edu>
2023-02-04 12:43:45 -0500

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int computerPrice = 1000;
+        System.out.println("What is the Price of your computer? ");
+        double saleTax = 0.05;
+        saleTax = computerPrice * saleTax;
+        System.out.println("The sales tax on your computer is "+ saleTax);
+        double fullPrice;
+        fullPrice = computerPrice + saleTax;
+                System.out.print("This is the total price of your comptuer "+ fullPrice);
+    }
+}
